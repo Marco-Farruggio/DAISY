99551977bec587ca3ffccc2ad81d2ca6475fd54a
Marco Farruggio <marcofarruggiopersonal@gmail.com>
2026-08-28 15:33:57 +0100

    Update eg.rs

--- examples/eg.rs
+++ examples/eg.rs
@@ -1,1 +1,30 @@
+//! API signature
+//!
+//! (Error handling is simplified
+//! for the sake of example)
+use daisy::{
+    Device,
+    devices,
+    Stream,
+    DeviceError,
+    StreamInitError,
+};
 
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let devs = devices()?;
+    let dev = devs.first()?;
+
+    let peaks = Arc::new([AtomicF32::new(0.0); 16]);
+    let stream = dev.stream(
+        0..=16,
+        0..=8,
+        move |inputs, outputs| {
+            for (i, channel) in inputs.iter().enumerate() {
+                peaks[i] = channel.max();
+            }
+            for channel in outputs {
+                channel.fill(0.0);
+            }
+        }
+    })?;
+}
