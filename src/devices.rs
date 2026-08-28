b145f9d38ba664cf59364450545b6fa5f7b04cbd
Marco Farruggio <marcofarruggiopersonal@gmail.com>
2026-08-28 13:34:36 +0100

    Update devices.rs

--- src/devices.rs
+++ src/devices.rs
@@ -1,1 +1,21 @@
+use crate::formats::ChannelCount;
+use crate::id::DeviceID;
 
+pub struct Device {
+    // placeholder for now...
+}
+
+impl Device {
+  // todo: these should be results really
+    pub fn id(&self) -> Option<DeviceId> {
+        None
+    }
+
+    pub fn name(&self) -> Option<String> {
+        None
+    }
+
+    pub fn input_channels(&self) -> Option<ChannelCount> {
+      None
+    }
+}
