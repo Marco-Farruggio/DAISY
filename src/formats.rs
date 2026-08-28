4a9c42228bc26faac6aa51457c000bb154c12ed4
Marco Farruggio <marcofarruggiopersonal@gmail.com>
2026-08-28 13:26:38 +0100

    Create formats.rs

---
+++ src/formats.rs
@@ -0,0 +1,38 @@
+//! Type definitions regarding
+//! stream formats
+//!
+//! Typing in github on mobile
+//! is very slow, I will check
+//! check this code when I am
+//! back home
+
+use std::num::NonZeroU16;
+
+pub enum DataType {
+    // Unsigned types
+    U8,
+    U16,
+    U24,
+    U32,
+    U48,
+    U64,
+
+    // Signed types
+    I16,
+    I24,
+    I32,
+    I48,
+    I64,
+
+    // Floating types
+    F32,
+    F64,
+
+    // DSD types (no DoP)
+    D64,
+    D128,
+}
+
+type SampleRate = u32;
+
+type ChannelCount = NonZeroU16
