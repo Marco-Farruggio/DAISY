51dcbf7acc0aa7ccedb753af6ff9ec221f023300
Marco Farruggio <marcofarruggiopersonal@gmail.com>
2026-08-28 15:56:24 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
+//! DAISY
+//! Duplex Audio Interface Stream API
+//!
+//! A Rust library for duplex audio
+//! on devices with ASIO drivers presenting COM interfaces.
+//!
+//! This is in no way related to the ASIO SDK by steinberg, and is legally excempt 
+//! from its licensing and legal restrictions.
+//!
+//! All code in this project is under the MIT license, though credit is appreciated.
+//!
+//! This project was inspired by hearing of JUCE's
+//! ability to handle multiple ASIO devices at once.
 
+use crate::devices::Device;
+use crate::errors::{DeviceError, StreamInitError};
+use crate::format::{DataType, SampleRate, ChannelCount};
