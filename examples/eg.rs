25c4034211861b74cbfd03ecd267beb901be75cc
Marco Farruggio <marcofarruggiopersonal@gmail.com>
2026-08-28 15:43:51 +0100

    Update eg.rs

--- examples/eg.rs
+++ examples/eg.rs
@@ -16,8 +16,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let peaks = Arc::new([AtomicF32::new(0.0); 16]);
     let stream = dev.stream(
-        0..=16,
-        0..=8,
+        0..16,
+        0..8,
         move |inputs, outputs| {
             for (i, channel) in inputs.iter().enumerate() {
                 peaks[i].set(channel.max());
